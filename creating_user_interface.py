1ff4163c9319873d9e342b303556b372a3c46f5e
HARISREE SURESH <getharisree@outlook.com>
2023-01-11 15:03:09 +0530

    Update creating_user_interface.py

--- creating_user_interface.py
+++ creating_user_interface.py
@@ -1,1 +1,13 @@
-test_model = pickle.load(open(
+test_model = pickle.load(open('image_model','rb')
+process_data = []
+directory = ''
+img = imread(directory)
+img = resize(image,(15,15))
+process_data.append(img.flatten())
+process_data = np.asarray(process_data)
+output = test_model.predict(process_data)
+output = categories[output[0]]
+                         
+print(output)
+
+                         
